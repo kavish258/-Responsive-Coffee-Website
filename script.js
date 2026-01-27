85a8e609987e0f24b8fbf469804bd238cb84ba99
kavish258 <Kavishonline0@gmail.com>
2026-01-27 21:05:58 +0530

    update code

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
+const menucloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 });
+
+menucloseButton.addEventListener("click", ()  => menuOpenButton.click()
+);
