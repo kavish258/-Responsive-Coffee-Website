3b79f773ceed992c5125e25c79334e603f903716
kavish258 <Kavishonline0@gmail.com>
2026-02-03 14:36:28 +0530

    code update and done

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menucloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 });
-
+// close menu when the close button is clicked
 menucloseButton.addEventListener("click", ()  => menuOpenButton.click()
 );
+// close menu when the nav link is clicked
+navLinks.forEach(link =>{
+    link.addEventListener("click", () =>  menuOpenButton.click() );
+});
 
 // Initialize Swiper
 
