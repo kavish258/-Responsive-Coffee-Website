0c2f429810c91b0015a01cb66d59c8effc63aa4f
kavish258 <Kavishonline0@gmail.com>
2026-02-03 13:05:15 +0530

    update code

--- script.js
+++ script.js
@@ -12,10 +12,14 @@ menucloseButton.addEventListener("click", ()  => menuOpenButton.click()
 
 const swiper = new Swiper('.slider-wrapper', {
   loop: true,
+  grabCurson: true,
+  spaceBetween: 25,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
   },
 
   // Navigation arrows
@@ -23,4 +27,18 @@ const swiper = new Swiper('.slider-wrapper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  // responsive breakpoints
+
+  breakpoints:{
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },
+  }
+
 });
